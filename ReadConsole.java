e3ffb70a7a5cd2f8761cd315dcba43e84d9c8f14
MITS <albynbabu97@gmail.com>
2018-03-24 14:58:29 +0530

    Readconsole

--- ReadConsole.java
+++ ReadConsole.java
@@ -1,19 +1,17 @@
 import java.io.*;
-public class ReadConsole {
-
-   public static void main(String args[]) throws IOException {
-      InputStreamReader cin = null;
-
-      try {
-         cin = new InputStreamReader(System.in);
-         System.out.println("Enter characters, 'q' to quit.");
-         char c;
-         do {
-            c = (char) cin.read();
-            System.out.print(c);
-         } while(c != 'q');
-      }catch(Exception e){}
-          cin.close();
+public class ReadConsole 
+{	
+	InputStreamReader cin = null;
+	public static void main(String args[]) throws IOException 
+	{
+		InputStreamReader cin = new InputStreamReader(System.in);
+         	System.out.println("Enter characters, 'q' to quit.");
+         	char c;
+         	do 
+		{
+            		c = (char) cin.read();
+            		System.out.print("\n"+c);
+         	} while(c != 'q');
+          	cin.close();
       }
-   }
 }
